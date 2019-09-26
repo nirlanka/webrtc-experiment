a313c21ad3cfb46451e293e008ebf511a68b9b89
Glitch (hello-express) <none>
2019-09-26 13:14:54 +0000

    💓🎌 Checkpoint ./server.js:288663/556 ./signal-manager.js:288663/603 ./public/style.css:288663/829 ./public/client.js:288663/1225 ./views/index.html:288663/1354

--- signal-manager.js
+++ signal-manager.js
@@ -5,5 +5,15 @@ const port = process.env.PORT2;
 const socket_server = new SocketServer({ port, });
 
 function init() {
-  
+  socket_server.on('connection', (socket) => {
+    socket.on('message', (msg) => {
+      console.log('received ' + msg);
+      
+      broadcast(socket_server, msg);
+    })
+  })
+}
+
+function broadcast(socket_server, data) {
+  socket_server.clients.forEach(client => client.send(data));
 }
