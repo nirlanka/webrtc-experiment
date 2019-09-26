b62fbe9e33876252068c7c8fbc8cc9db6c046a44
Glitch (hello-express) <none>
2019-09-26 15:27:08 +0000

    🏀🖖 Checkpoint ./public/client.js:288663/440

--- public/client.js
+++ public/client.js
@@ -1,17 +1,20 @@
 const server_connection = new WebSocket('ws://[::1]:4000');
-server_connection.onmessage = () => {}
+server_connection.onmessage = msg => {
+  if (!peer_connection)
+    start()
+}
 
 let local_stream;
 
 navigator.getUserMedia(
   { video: true, audio: true, }, 
+  
   stream => {
     local_stream = stream;
     local_video.src = window.URL.createObjectURL(stream);
   },
-  err => {
-    console.error('Failed getting user video. ', err);
-  }
+  
+  err => console.error('Failed getting user video. ', err)
 )
 
 const peer_connection_config = {
@@ -31,42 +34,26 @@ function start_stream(is_caller) {
   peer_connection = new RTCPeerConnection(peer_connection_config);
   
   peer_connection.onicecandidate = event => {
-    if (event.candidate) {
-      const ice_json = JSON.stringify({ ice: event.candidate });
-      server_connection.send(ice_json);
-    }
+    if (event.candidate) 
+      server_connection.send(JSON.stringify({ ice: event.candidate }));
   }
   
-  peer_connection.onaddstream = event => {
-    console.log('Got ')
-  }
+  peer_connection.onaddstream = event => console.log('Got ');
   
   peer_connection.addStream(local_stream);
   
-  if (is_caller) {
-    
+  if (is_caller) 
     peer_connection.createOffer(
       description => {
         console.log('Got description');
         
         peer_connection.setLocalDescription(
           description, 
-          
-          () => {
-            const desc_json = JSON.stringify({ sdp: description });
-            server_connection.send(desc_json);
-          },
-          
-          () => {
-            console.error('Failed to set description.');
-          }
+          () => server_connection.send(JSON.stringify({ sdp: description })),
+          () => console.error('Failed to set description.')
         )
       },
       
-      err => {
-        console.error('Failed to create offer. ', err);
-      }
+      err => console.error('Failed to create offer. ', err)
     )
-    
   }
-}
