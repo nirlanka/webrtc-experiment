ce58579e643d8cf10ae067c2fb9281502329a27e
Glitch (hello-express) <none>
2019-09-26 13:26:08 +0000

    📼😎 Checkpoint ./public/client.js:288663/923 ./views/index.html:288663/278

--- public/client.js
+++ public/client.js
@@ -1,1 +1,21 @@
-let local_video, remote_video, ;
+let peer_connection;
+
+const peer_conn_config = {
+  ice_servers: [
+    { url: 'stun.l.google.com:19302' },
+    { url: 'stun1.l.google.com:19302' },
+    { url: 'stun2.l.google.com:19302' },
+  ]
+}
+
+const constraints = {
+  video: true,
+  
+}
+
+const local_video = document.getElementById('local-video');
+const remote_video = document.getElementById('remote-video');
+
+const server_connection = new WebSocket('ws://[::1]:4000');
+server_connection.onmessage = () => {}
+
