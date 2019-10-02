924c4096abb99815016d00a778e213e7ec253618
Glitch (hello-express) <none>
2019-10-02 11:19:03 +0000

    🍔👴 Checkpoint ./public/client.js:288663/235

--- public/client.js
+++ public/client.js
@@ -1,1 +1,7 @@
-const peer_connection = new RTCPeerConnection();
+const ice_servers = [
+  {
+    url: 'stun:stun.l.google.com:19302',
+  },
+]
+
+const peer_connection = new RTCPeerConnection(ice_servers);
