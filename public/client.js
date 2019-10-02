7fa1a64513efaf6c81e579030c49e2e97a74fef1
Glitch (hello-express) <none>
2019-10-02 13:04:03 +0000

    💿😅 Checkpoint ./public/client.js:288663/1970

--- public/client.js
+++ public/client.js
@@ -1,3 +1,5 @@
+// Connections
+
 const config = {
   iceServers: [
     {
@@ -12,6 +14,8 @@ const remote_connection = new RTCPeerConnection(config);
 local_connection.onicecandidate = e => remote_connection.addIceCandidate(e.candidate);
 remote_connection.onicecandidate = e => local_connection.addIceCandidate(e.candidate);
 
+// Local channel
+
 const local_channel = local_connection.createDataChannel('messaging-channel');
 
 let is_connected;
@@ -20,6 +24,8 @@ const local_messages = [];
 local_channel.onopen = () => is_connected = true;
 local_channel.onclose = () => is_connected = false;
 local_channel.onmessage = e => local_messages.push(e.data);
+
+// Remote channel
 
 let remote_channel;
 let remote_messages = [];
@@ -30,6 +36,24 @@ remote_connection.ondatachannel = e => {
   remote_channel.onmessage = e => remote_messages.push(e.data);
   remote_channel.onclose = () => is_connected = false;
 }
+
+async function init_offers() {
+  // Local offer
+
+  const local_offer = await local_connection.createOffer();
+  local_connection.setLocalDescription(local_offer).then();
+  remote_connection.setRemoteDescription(local_offer).then();
+
+  // Remote answer
+
+  const remote_answer = await remote_connection.createAnswer();
+  await local_connection.setRemoteDescription(remote_answer).then();
+  await remote_connection.setLocalDescription(remote_answer).then();
+}
+
+init_offers();
+
+// UI
 
 function onclick_send_message() {
   local_channel.send('Lorem ipsum dolor sit amet.');
