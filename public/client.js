a7385336b2a72f863c826446a79c31fb38be6a7c
Glitch (hello-express) <none>
2019-10-02 12:29:03 +0000

    💆🌶 Checkpoint ./public/client.js:288663/842

--- public/client.js
+++ public/client.js
@@ -7,8 +7,35 @@ const ice_servers = [
 const peer_connection = new RTCPeerConnection(ice_servers);
 const channel = peer_connection.createDataChannel('meta');
 
-channel.onmessage = event => console.log('>> received', event.data);
+channel.onmessage = event => {
+  console.log('received', event.data);
+  
+  const data = JSON.parse(event.data);
+  
+  switch (data.type) {
+    case 'login':
+      on_login(data);
+      break;
+    case 'offer':
+      //
+      break;
+    case 'reply':
+      //
+      break;
+    case 'candidate':
+      //
+      break;
+  }
+}
 
-channel.onopen = event => console.log('>> channel open');
+channel.onopen = event => console.log('channel open');
 
-channel.onclose = event => console.log('>> channel close');
+channel.onclose = event => console.log('channel close');
+
+function on_login(data) {
+  if (data.is_accepted) {
+    console.log('failed login');
+  } else {
+    // 
+  }
+}
