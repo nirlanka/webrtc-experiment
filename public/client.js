d4193633bba1a8bee916fdc01d58c72a06c4dbde
Glitch (hello-express) <none>
2019-10-02 12:41:03 +0000

    🐉💘 Checkpoint ./public/client.js:288663/1897

--- public/client.js
+++ public/client.js
@@ -1,36 +1,32 @@
-const ice_servers = [
-  {
-    url: "stun:stun.l.google.com:19302"
-  },
-];
+const config = {
+  iceServers: [
+    {
+      url: "stun:stun.l.google.com:19302"
+    },
+  ]
+};
 
-const peer_connection = new RTCPeerConnection(ice_servers);
-const channel = peer_connection.createDataChannel('meta');
+const local_connection = new RTCPeerConnection(config);
+const local_channel = local_connection.createDataChannel('messagi-channel');
 
-channel.onmessage = event => {
+const remote_connection = new RTCPeerConnection(config);
+const remote_channel = remote_connection.createDataChannel('meta');
+
+local_channel.onicecandidate = event => ;
+
+local_channel.onmessage = event => {
   console.log('received', event.data);
   
   const data = JSON.parse(event.data);
   
-  switch (data.type) {
-    case 'login':
-      on_login(data);
-      break;
-    case 'offer':
-      //
-      break;
-    case 'reply':
-      //
-      break;
-    case 'candidate':
-      //
-      break;
+  if (data.type === 'login') {
+    // 
   }
 }
 
-channel.onopen = event => console.log('channel open');
+local_channel.onopen = event => console.log('channel open');
 
-channel.onclose = event => console.log('channel close');
+local_channel.onclose = event => console.log('channel close');
 
 function on_login(data) {
   if (data.is_accepted) {
