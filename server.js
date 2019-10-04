15cc822fa1cecb09cc83a3918acfdc5edc32d02d
Glitch (hello-express) <none>
2019-10-04 06:46:07 +0000

    🕌🎠 Checkpoint ./server.js:288663/2

--- server.js
+++ server.js
@@ -16,5 +16,4 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-app.listen(process.env.PORT, () => 
- console.log('Your app is listening on port ' + process.env.PORT));
+app.listen(process.env.PORT, () => console.log('Your app is listening on port ' + process.env.PORT));
